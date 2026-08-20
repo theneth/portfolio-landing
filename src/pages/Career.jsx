4bc54592ff7db915722e6e654cdf9fa21c983c4f
Theneth Sanjuka <stheneth@gmail.com>
2026-08-20 11:29:19 +0530

    Fix LuCheckCircle import error

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,4 +1,4 @@
-import { LuBriefcase, LuMapPin, LuCalendar, LuCheckCircle2, LuTerminal, LuFolderGit2 } from "react-icons/lu";
+import { LuBriefcase, LuMapPin, LuCalendar, LuCheckCircle, LuTerminal, LuFolderGit2 } from "react-icons/lu";
 
 const careerData = [
   {
@@ -149,7 +149,7 @@ export default function Career() {
                     <ul className="achievement-list">
                       {company.responsibilities.map((resp, i) => (
                         <li key={i} className="achievement-item">
-                          <LuCheckCircle2 className="achievement-icon" aria-hidden="true" />
+                          <LuCheckCircle className="achievement-icon" aria-hidden="true" />
                           <span>{resp}</span>
                         </li>
                       ))}
